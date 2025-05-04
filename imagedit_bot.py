354e3a6f470a3e4706f0bba8a9da5349916c4a22
its-mezeus <hishammuhammed0011@gmail.com>
2025-05-04 20:39:52 +0530

    Update imagedit_bot.py

--- imagedit_bot.py
+++ imagedit_bot.py
@@ -4,13 +4,13 @@ from flask import Flask
 from threading import Thread
 from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
 
-# BOT TOKEN
+# Bot token
 bot = telebot.TeleBot("6014279190:AAEpUHeY6eFStBFAQiYVs8kTpezfJtWVEs4")
 
-# FIXED IMAGE URL
+# Fixed reply image
 REPLY_IMAGE_URL = "https://envs.sh/lg2.jpg"
 
-# Flask keep-alive server
+# Flask server for keep-alive
 app = Flask('')
 
 @app.route('/')
@@ -24,7 +24,7 @@ def keep_alive():
     t = Thread(target=run)
     t.start()
 
-# START command handler
+# Start command
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
     user_name = message.from_user.first_name or "there"
@@ -37,24 +37,22 @@ def send_welcome(message):
     markup.add(InlineKeyboardButton("Owner 🙂", callback_data="owner_info"))
     bot.send_message(message.chat.id, welcome_text, parse_mode="HTML", reply_markup=markup)
 
-# Handle image messages
+# Image handler
 @bot.message_handler(content_types=['photo'])
 def handle_photo(message):
     wait_msg = bot.reply_to(message, "Wait Let me Cook.... 💓")
     time.sleep(5)
     try:
         bot.delete_message(chat_id=message.chat.id, message_id=wait_msg.message_id)
-    except Exception as e:
-        print("Delete failed:", e)
+    except:
+        pass
     bot.send_photo(message.chat.id, photo=REPLY_IMAGE_URL)
 
-# Handle inline button callback
+# Button callback
 @bot.callback_query_handler(func=lambda call: call.data == "owner_info")
 def callback_owner(call):
     bot.answer_callback_query(call.id, text="Arinjitt Enthina 😂", show_alert=True)
 
-# Run keep-alive server
+# Start Flask + Bot
 keep_alive()
-
-# Start the bot
 bot.polling()
